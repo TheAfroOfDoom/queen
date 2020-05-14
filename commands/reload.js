dab79efcee70b31ebb1a78b72c350df9a8068a65
TheAfroOfDoom <tehafroofdoom@gmail.com>
2020-05-14 05:35:32 -0400

    Refactored for ability to call function externally

--- commands/reload.js
+++ commands/reload.js
@@ -5,11 +5,14 @@ module.exports = {
     async execute(msg, args, client) {
         module.exports.reloadMessage = await msg.channel.send("Reloading...")
             .then(message => {return message;})
-            .catch(console.error);;
-        //vars = ["prefix", "token", "commandChannels", "guildId", "serverId", "aliases", "callProperties"]
+            .catch(console.error);
+        
+        module.exports.reload();
+    },
+    reload() {
         delete require.cache[require.resolve('./../config.json')];  
         module.exports.config = require('./../config.json');
         module.exports.config.callProperties.maxAge = module.exports.config.callProperties.maxAge.split(" ");     // '2 hours' => ['2', 'hours']
         module.exports.args = [null, "reload"];
-    },
+    }
 };
