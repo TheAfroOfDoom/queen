5609c8898cc1e59644c680ecc5cd073d3c128a3d
TheAfroOfDoom <tehafroofdoom@gmail.com>
2020-05-15 04:22:28 -0400

    Added //alias get and //alias add.

--- commands/alias.js
+++ commands/alias.js
@@ -13,99 +13,213 @@ module.exports = {
         } catch {
             badArgument(0, msg, args, client);
         }
-
-        client.commands.get("reload").reload();
     },
 };
 
 async function badArgument(pos, msg, args, client, path = "", scold = true) {
-    tree = [
-        'list',
-        'add',
-        'get',
-        'remove',
-        'set'
-    ];
-
+    r = '';
     if(scold) {
-        s = `\`Bad argument in position ${pos + 1}: ${args[pos]}.\``;
-        await msg.channel.send(s);
+        r = `Bad argument in position ${pos + 1}: \`${args[pos]}\`.\n`;
     }
 
-    path = `\`Usage: ${module.exports.name} ` + path;
-    if(pos == 0) {
-        s = path + `<`;
-        for(t of tree) {
-            s += `${t}|`;
-        }
-        s = s.slice(0, s.length - 1) + '>`';   // slice() => Remove extra '|'
-        await msg.channel.send(s);
-    } if(pos == 1) {
-        x = path.split(" ");
-        switch(x[x.length - 1]) {
-            case "get":
-                s = path + ` <albionUsername|discordID|discordMention>`;
-                s += `\nInvalid argument or player does not have an entry in aliases.\``;
-                await msg.channel.send(s);
-                break;
-            case "set":
-                s = path + ` <albionUsername|discordID|discordMention>`
-        }
+    path = `Usage: \`${module.exports.name} ` + path;
+    x = path.split(" "), s = `\n\n` + path;
+    switch(pos) {
+        case 0:
+            s = s.slice(1, s.length);
+            s += `<`;
+            tree = [
+                'list',
+                'add',
+                'get',
+                'remove',
+                'set'
+            ];
+
+            for(t of tree) {
+                s += `${t}|`;
+            }
+            s = s.slice(0, s.length - 1) + '>`';   // slice() => Remove extra '|'
+            break;
+    
+        case 1:
+            switch(x[x.length - 1]) {
+                case "get":
+                    s += ` <albionUsername|discordID|discordMention>\``;
+                    if(args.length >= 2) {
+                        s = `Invalid argument or player does not have an entry in alias list.` + s;
+                    } else {
+                        s = `No player specified.` + s;
+                    }
+                    break;
+
+                case "add":
+                    s += ` <albionUsername> <discordID|discordMention>\``;
+                    switch(args[0]) {
+                        case 'badAlbion':
+                            s = `\`\`\`\nAlbion usernames must only contain alphanumeric characters, dashes, and underscores. Must be between 3-16 characters long.\`\`\`` + s.slice(1, s.length);
+                            break;
+
+                        case 'noAlbion':
+                            s = `Albion user not specified.` + s;
+                            break;
+
+                        case 'alreadyExistAlbion':
+                            config = require("./../config.json");
+                            albionUsername = args[1];
+                            s = `Albion username \`${albionUsername}\` already attached to ${(await msg.guild.members.fetch(config.aliases[albionUsername])).user.tag}.` + s;
+                            break;
+                    }
+                    break;
+            }
+            break;
+
+        case 2:
+            switch(x[x.length - 2]) {
+                case "add":
+                    s += ` <discordID|discordMention>\``;
+                    switch(args[0]) {
+                        case 'badDiscord':
+                            s = `Argument is neither a Discord ID nor a mention.` + s;
+                            break;
+
+                        case 'notFoundDiscord':
+                            s = `Discord user not found.` + s;
+                            break;
+
+                        case 'noDiscord':
+                            s = `Discord user not specified.` + s;
+                            break;
+                    }
+                    break;
+            }
+            break;
     }
+    await msg.channel.send(r + s);
 }
 
 async function list(msg, args, client) {
     config = require("./../config.json");
 
     s = "```\n";
-    for (const [key, value] of Object.entries(config.aliases)) {
-        s += `${key}: ${value}\n`;
+    for (const [albionUsername, discordID] of Object.entries(config.aliases)) {
+        s += `${albionUsername}: ${discordID}`;
+
+        // This is super laggy. \/
+        /*
+        if(args.length > 1 && (args[1] == 'tags' || args[1] == 'tag')) {
+            try {   // While testing, Discord user might not be in guild anymore.
+                userTag = (await msg.guild.members.fetch(discordID)).user.tag;
+                s += ` (${userTag})`;
+            } catch {}
+        }
+        */
+        s += `\n`;
     }
     await msg.channel.send(s + "```");
 }
 
 async function add(msg, args, client) {
-    
+    // No Albion name specified in args.
+    if(args.length <= 1) {
+        args[0] = 'noAlbion';
+        badArgument(1, msg, args, client, "add");
+        return;
+    }
+
+    // Ensure Albion name matches game requirements:
+    // "Albion usernames must only contain alphanumeric characters, dashes, and underscores. Must be between 3-16 characters long."
+    albionUsername = args[1].toLowerCase(), regex = new RegExp(`^[\\w\-]{3,16}$`);
+    if(!regex.test(albionUsername)) {
+        args[0] = 'badAlbion';
+        badArgument(1, msg, args, client, "add");
+        return;
+    }
+
+    // Ensure Albion name doesn't already exist in list.
+    config = require("./../config.json");
+    if(Object.keys(config.aliases).includes(albionUsername)) {
+        args[0] = 'alreadyExistAlbion', args[1] = albionUsername;
+        badArgument(1, msg, args, client, "add");
+        return;
+    }
+
+    // No Discord user specified in args.
+    if(args.length <= 2) {
+        args[0] = 'noDiscord';
+        badArgument(2, msg, args, client, `add ${albionUsername}`);
+        return;
+    }
+
+    // Ensure Discord user exists in Discord server:
+    discordUser = args[2].replace("!", "").replace("<@", "").replace(">", "");
+    try {
+        regexID = new RegExp(`^\\d{0,18}$`);            // Discord ID format
+
+        if(regexID.test(discordUser)) {
+            discordUser = (await msg.guild.members.fetch(discordUser)).user;
+        } else {
+            throw "Didn't match regex";
+        }
+    } catch {
+        args[0] = 'badDiscord';
+        badArgument(2, msg, args, client, `add ${albionUsername}`);
+        return;
+    }
+
+    // Discord user not found in guild
+    if(discordUser == undefined) {
+        args[0] = 'notFoundDiscord';
+        badArgument(2, msg, args, client, `add ${albionUsername}`);
+        return;
+    }
+
+    // NOTE(jordan): At this point we know that the Albion name and Discord user are valid.
+    // TODO(jordan): If Discord user is already attached to Albion name(s), prompt for confirmation to add new alias.
+
+    // Add alias to list
+    config.aliases[albionUsername] = discordUser.id;
+
+    // Save and reload new config
+    await client.commands.get("reload").writeConfig(config);
+
+    // Show confirmation of write
+    s = `Added alias: \`<${albionUsername}: ${discordUser.id}>\` (${discordUser.tag}).`;
+    await msg.channel.send(s);
 }
 
 async function get(msg, args, client) {
-    config = require("./../config.json");
-
-    try {
-        player = args[1].toLowerCase();
-        //console.info(Object.keys(config.aliases).includes(player));
-        has = Object.keys(config.aliases).includes(player);
-        
-        // If input string is in the form of a Discord ID (ex: '167762537989799937')
-        if(RegExp(`^\\d{0,18}$`).test(player) && player.length <= 18) {
-            has = has || Object.values(config.aliases).includes((await msg.guild.members.fetch(player)).user.id);
-        }
-        
-        // If input string is in the form of a Discord Mention (ex: '<@!167762537989799937>')
-        if(RegExp(`^<@!\\d{0,18}>$`).test(player) && player.length <= 22) {
-            player = player.slice(3, player.length - 1);    // Grab just the ID part
-            has = has || Object.values(config.aliases).includes((await msg.guild.members.fetch(player)).user.id);
-        }
-
-        if(!has) {
-            // arg was either not an Albion username or Discord mention in the list
-            badArgument(1, msg, args, client, "get");
-            return;
-        }
-    } catch (e) {
-        // bad arg
+    if(args.length <= 1) {
         badArgument(1, msg, args, client, "get");
         return;
     }
 
-    s = "`";
+    config = require("./../config.json");
+    player = args[1].toLowerCase();
+
+    x = undefined;
+    try {
+        x = player.replace("!", "").replace("<@", "").replace(">", "");    // Nicked users are '<@!...>', while unnicked users are '<@...>'.
+    } catch {}
+
+    s = '', found = false, discordUser = undefined;
     for(const [albionUsername, discordID] of Object.entries(config.aliases)) {
-        if(player == albionUsername || player == discordID || player.split("<@!").slice(player.length - 1) == discordID) {
-            s += `Found entry: <${albionUsername}: ${discordID}>\``;
-            break;
+        if(player == albionUsername || player == discordID || x == discordID) {
+            found = true;
+            discordUser = discordID;
+            s += `\t\`<${albionUsername}: ${discordID}>\`\n`;
         }
     }
-    await msg.channel.send(s);
+    if(found) {
+        userTag = 'Found: ';
+        try {   // While testing, Discord user might not be in server.
+            userTag = (await msg.guild.members.fetch(discordUser)).user.tag;
+            userTag = `Found: ${userTag}`;
+        } catch {}
+        await msg.channel.send(`${userTag}\n${s}`);
+        return;
+    }
+    badArgument(1, msg, args, client, "get");
 }
 
 function remove(msg, args, client) {
--- commands/reload.js
+++ commands/reload.js
@@ -14,5 +14,26 @@ module.exports = {
         module.exports.config = require('./../config.json');
         module.exports.config.callProperties.maxAge = module.exports.config.callProperties.maxAge.split(" ");     // '2 hours' => ['2', 'hours']
         module.exports.args = [null, "reload"];
+    },
+    async writeConfig(config) {
+        // Sort aliases alphabetically: https://stackoverflow.com/a/31102605
+        const aliasesOrdered = {};
+        Object.keys(config.aliases).sort().forEach(function(key) {
+            aliasesOrdered[key] = config.aliases[key];
+        });
+        config.aliases = aliasesOrdered;
+
+        // Merge config.callProperties.maxAge back into a string
+        config.callProperties.maxAge = `${config.callProperties.maxAge[0]} ${config.callProperties.maxAge[1]}`;
+
+        // Write to disk
+        const fs = require('fs');
+        const util = require('util');
+        writeFile = util.promisify(fs.writeFile);
+        await writeFile("./config.json", JSON.stringify(config, null, 4), function(err) {
+            if(err) throw err;
+        });
+
+        module.exports.reload();
     }
 };
