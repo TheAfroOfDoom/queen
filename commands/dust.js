36cc6c1105651d4be743b6d651bc0b2a547520bf
TheAfroOfDoom <tehafroofdoom@gmail.com>
2020-05-21 23:28:17 -0400

    Removed //dust.

--- commands/dust.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'dust',
-    aliases: ['d'],
-    description: 'Bites the dust.',
-    async execute(msg, args, client) {    
-        m = 'bite my dust :) <@' + msg.author.id + '>';
-        msg.channel.send(m);
-    },
-};
